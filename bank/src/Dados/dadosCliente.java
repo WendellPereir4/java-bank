b4afa39c390ef8bc80e61ead1b3bd509282ebffc
wendell <wendellluciopereira4080@gmail.com>
2026-08-30 19:56:22 -0300

    criando classe dados do cliente

---
+++ bank/src/Dados/dadosCliente.java
@@ -0,0 +1,20 @@
+package Dados;
+
+public class dadosCliente {
+
+    private String nomeDoCliente;
+    private int idadeDoCliente;
+    private int cpfDoCliente;
+
+    void CriarContaBanco(){
+        System.out.println(nomeDoCliente);
+        System.out.println(idadeDoCliente);
+        System.out.println(cpfDoCliente);
+
+
+    }
+
+
+
+
+}
