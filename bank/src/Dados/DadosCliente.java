2c5ec9f47593f51fe5758edc1657e84fd429fac5
wendell <wendellluciopereira4080@gmail.com>
2026-08-31 20:21:50 -0300

    coletando dados do cliente para imprimir

---
+++ bank/src/Dados/DadosCliente.java
@@ -0,0 +1,32 @@
+package Dados;
+
+public class DadosCliente {
+
+    private String nomeDoCliente;
+    private int idadeDoCliente;
+    private Long cpfDoCliente;
+
+    public    DadosCliente(String nomeDoCliente, int idadeDoCliente, Long cpfDoCliente){// CONSTRUTOR, recebe os dados da main.
+
+        this.nomeDoCliente = nomeDoCliente;
+        // this.nomeDoCliente -> atributo da classe (fica guardado no objeto)
+        // nomeDoCliente -> parâmetro recebido pelo construtor
+        this.idadeDoCliente = idadeDoCliente;
+        this.cpfDoCliente = cpfDoCliente;
+    }
+
+    public String getNomeDoCliente(){
+        return nomeDoCliente;
+        // Método getter: retorna o valor do atributo private nomeDoCliente.
+        // return nomeDoCliente; devolve esse valor pra quem chamou.
+    }
+
+    public int getIdadeDoCliente(){
+        return idadeDoCliente;
+    }
+
+    public Long getCpfDoCliente(){
+        return cpfDoCliente ;
+    }
+
+}
---
+++ bank/src/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Dados.DadosCliente;
+
+public class Main {
+  static void main(String[] args) {
+
+    DadosCliente cliente = new DadosCliente("Wendell",22, 24156365840L);
+
+    //Car car = new Car("BMW", "X6", 2024, 125000+12500); //-> new cria objetos, exem: BMW, x6, 2024. tipo uma pessoa fisica colocar idade,nome
+
+  }
+}
